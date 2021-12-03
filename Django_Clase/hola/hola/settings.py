f37b37cb7843d25a99ead78e8c09339b14d63ee1
JocelynHC <jocelyn.cruz@estudiante.uacm.edu.mx>
2021-12-03 01:11:16 -0600

    Commit

--- Django_Clase/hola/hola/settings.py
+++ Django_Clase/hola/hola/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-     'viernes', 'principal',
+     'viernes', 
+     'principal',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
