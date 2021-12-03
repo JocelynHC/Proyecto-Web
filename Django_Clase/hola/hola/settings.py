2766134445e911cd845713de5a12aec83b9502b7
JocelynHC <jocelyn.cruz@estudiante.uacm.edu.mx>
2021-12-03 01:23:51 -0600

    Cambios

--- Django_Clase/hola/hola/settings.py
+++ Django_Clase/hola/hola/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-     'viernes', 
+     
      'principal',
     'django.contrib.admin',
     'django.contrib.auth',
--- Django_Clase/hola/hola/urls.py
+++ Django_Clase/hola/hola/urls.py
@@ -19,5 +19,4 @@ from django.urls import  path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('principal/', include('principal.urls')),
-    path('viernes/'), include('viernes.urls'),
 ]
