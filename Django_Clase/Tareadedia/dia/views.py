978cccbf671bca3aae903865ece91ee70e470dcd
JocelynHC <jocelyn.cruz@estudiante.uacm.edu.mx>
2021-12-03 03:22:26 -0600

    Es viernes version 2.1

--- Django_Clase/Tareadedia/dia/views.py
+++ Django_Clase/Tareadedia/dia/views.py
@@ -4,5 +4,6 @@ from datetime import datetime
 # Create your views here.
 def index(request):
     return render(request, 'dia/index.html', {'esViernes':datetime.today().isoweekday() ==5
+    #'esViernes':'Ya es viernes' if datetime.today().isoweekday() ==2 else 'No es viernes'
     })
 
