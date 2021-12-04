53635857951eca7ec84b00cdae36b09f4dd48eae
JocelynHC <jocelyn.cruz@estudiante.uacm.edu.mx>
2021-12-03 22:44:55 -0600

    Alarmas layout

--- Django_Clase/hola/Alarmas/views.py
+++ Django_Clase/hola/Alarmas/views.py
@@ -5,3 +5,6 @@ Alarmas= ['5:30', '5:35', '5:40', '5:50']
 # Create your views here.
 def index(request):
     return render(request, 'Alarmas/index.html', {'Alarmas':Alarmas})
+
+def v2(request):
+    return render(request, 'Alarmas/v2.html')
