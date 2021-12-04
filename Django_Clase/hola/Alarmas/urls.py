312d52281ea57f224add895f9182f823c34cdc1c
JocelynHC <jocelyn.cruz@estudiante.uacm.edu.mx>
2021-12-03 23:19:58 -0600

    Alarmas version 2 y 2.1

--- Django_Clase/hola/Alarmas/urls.py
+++ Django_Clase/hola/Alarmas/urls.py
@@ -2,7 +2,7 @@
 from django.urls import  path
 from . import views
 
-
+app_name="Alarmas"
 urlpatterns = [
     path("", views.index, name="index"),
     path("v2", views.v2, name="v2"),
--- Django_Clase/hola/principal/urls.py
+++ Django_Clase/hola/principal/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name='principal'
 urlpatterns=[
     path("", views.holaDjango, name="holaDjango"),
     path('pepe', views.pepe, name="holapepe"),
